593546a3b30dec8ba9ea6405ab4c1dbaa464e962
Eternal Junior <95535122+0-EternalJunior-0@users.noreply.github.com>
2024-07-29 14:16:25 +0300

    Update ConvertVideoFormat.py

--- ConvertVideoFormat.py
+++ ConvertVideoFormat.py
@@ -3,13 +3,10 @@ from moviepy.editor import VideoFileClip
 
 def convert_video_format(input_file, output_file, target_resolution=(1920, 1080), target_format='mp4'):
     try:
-        # Відкрийте відеофайл
         video_clip = VideoFileClip(input_file)
 
-        # Змініть розмір відео
         resized_clip = video_clip.resize(newsize=(target_resolution[0], target_resolution[1]))
 
-        # Конвертуємо в інший формат
         resized_clip.write_videofile(output_file, codec='libx264', fps=video_clip.fps, audio=False)
 
         print(f"Конвертація відео завершена: {output_file}")
@@ -18,11 +15,9 @@ def convert_video_format(input_file, output_file, target_resolution=(1920, 1080)
         print(f"Помилка під час конвертації відео: {e}")
 
 if __name__ == "__main__":
-    # Замініть це на ваші шляхи та імена файлів
     input_video_path = "2036944935.mp4"
     output_video_path = "output_video.mp4"
 
-    # Вказати розмір вихідного відео (за замовчуванням 1920x1080)
     target_resolution = (1920, 1080)
 
     # Конвертація відео до потрібного формату та розміру
